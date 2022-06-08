b3b61d4f6b313d6b66ca234b0284b2296d5b32d9
DeciPaliz <unat7919@gmail.com>
2022-06-08 14:21:12 +0500

    added the submit button

--- src/letterbutton.js
+++ src/letterbutton.js
@@ -1,13 +1,25 @@
 var LetterButton = cc.Node.extend({
     ctor: function (letter) {
         this._super();
+
+        this.onClick = function () {};
         
         this.letter = letter;
         this.letter_sprite = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame(resources.letters_dict[this.letter]));
-        this.letter_bg = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame('letter_bg.png'));
+//        this.letter_bg = new ccui.Button(cc.spriteFrameCache.getSpriteFrame('letter_bg.png'));
+        this.letter_bg = new ccui.Button('#letter_bg.png', null, null, ccui.Widget.PLIST_TEXTURE);
         this.letter_sprite.setLocalZOrder(2);
         this.letter_bg.setLocalZOrder(1);
+        this.letter_bg.setZoomScale(0);
         this.addChild(this.letter_sprite);
         this.addChild(this.letter_bg);
+
+        this.selectionBox = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame('letter_bg_glow.png'));
+        this.selectionBox.setVisible(false);
+        this.addChild(this.selectionBox);
+
+        this.letter_bg.addClickEventListener(function () {
+            this.onClick();
+        }.bind(this));
     },
 });
--- src/letterwheel.js
+++ src/letterwheel.js
@@ -5,11 +5,24 @@ var LetterWheel = cc.Node.extend({
         this.letters = letters;
         this.radius = radius;
         this.letterbuttons = new Array(this.letters.length).fill();
-        for (let i = 0; i < this.letters.length; i++)
+        for (let i = 0; i < this.letters.length; i++) {
             this.letterbuttons[i] = new LetterButton(this.letters[i]);
-        for (let lb of this.letterbuttons)
-            this.addChild(lb);
+            this.letterbuttons[i].onClick = this.onLetterButtonClick.bind(this, i);
+            this.addChild(this.letterbuttons[i]);
+        }
         this.setCirclePosition();
+        this.selected = new Array(this.letters.length).fill(false);
+        this.selectedCount = 0;
+
+        this.submitButton = new ccui.Button('#button_blue.png', '#button_blue_on.png', null, ccui.Widget.PLIST_TEXTURE);
+        this.submitButtonIcon = new cc.Sprite(cc.spriteFrameCache.getSpriteFrame('submit_icon.png'));
+        this.submitButton.setScale9Enabled(true);
+        this.submitButton.setCapInsets(cc.rect(this.submitButton.width/2 - 1, this.submitButton.height/2 - 1, 2, 2));
+        this.submitButton.setContentSize(this.submitButtonIcon.width + 30, this.submitButtonIcon.height + 20);
+        this.submitButton.setVisible(false);
+        this.submitButtonIcon.setVisible(false);
+        this.addChild(this.submitButton);
+        this.addChild(this.submitButtonIcon);
     },
 
     setCirclePosition: function () {
@@ -17,6 +30,20 @@ var LetterWheel = cc.Node.extend({
         for (let i = 0; i < this.letters.length; i++) {
             let lb = this.letterbuttons[i];
             lb.setPosition(this.x + Math.floor(this.radius*(Math.sin(i*angle))), this.y + Math.floor(this.radius*(Math.cos(i*angle))));
+        }
+    },
+
+    onLetterButtonClick: function (index) {
+        this.letterbuttons[index].selectionBox.setVisible(!this.letterbuttons[index].selectionBox.isVisible());
+        this.selectedCount += this.letterbuttons[index].selectionBox.isVisible() ? 1 : -1;
+        if (this.selectedCount >= 3) {
+            //TODO: animation
+            this.submitButton.setVisible(true);
+            this.submitButtonIcon.setVisible(true);
+        }
+        else {
+            this.submitButton.setVisible(false);
+            this.submitButtonIcon.setVisible(false);
         }
     },
 });
